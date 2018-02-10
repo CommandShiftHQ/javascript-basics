06326963f9168acc4de654a55260b86332c9ef29
Michael Harrison <harrim91@hotmail.co.uk>
2018-02-10 20:13:28 +0000

    Add array and object functions and tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -16,7 +16,6 @@ import {
   countCharacters,
   firstCharacter,
   firstCharacters,
-  removeSpaces,
   negate,
   both,
   either,
@@ -32,7 +31,33 @@ import {
   startsWith,
   containsVowels,
   isLowerCase,
-} from '../src/solutions';
+  getType,
+  getNthElement,
+  arrayToCSVString,
+  csvStringToArray,
+  addToArray,
+  addToArray2,
+  removeNthElement,
+  numbersToStrings,
+  uppercaseWordsInArray,
+  reverseWordsInArray,
+  onlyEven,
+  removeNthElement2,
+  elementsStartingWithAVowel,
+  removeSpaces,
+  sumNumbers,
+  sortByLastLetter,
+  createPerson,
+  getName,
+  getProperty,
+  hasProperty,
+  isOver65,
+  getAges,
+  findByName,
+  findHondas,
+  averageAge,
+  createTalkingPerson,
+} from '../src';
 
 describe('add', () => {
   it('adds the two numbers together', () => {
@@ -175,13 +200,6 @@ describe('firstCharacters', () => {
   });
 });
 
-describe('removeSpaces', () => {
-  it('returns the string with the space characters removed', () => {
-    expect(removeSpaces('this string has spaces')).toEqual('thisstringhasspaces');
-    expect(removeSpaces(' this one has sneaky   spaces ')).toEqual('thisonehassneakyspaces');
-  });
-});
-
 describe('negate', () => {
   it('returns the opposite of the passed boolean value', () => {
     expect(negate(true)).toBe(false);
@@ -317,3 +335,324 @@ describe('isLowerCase', () => {
   });
 });
 
+describe('getType', () => {
+  it('returns the type of the value', () => {
+    expect(getType('foo')).toEqual('string');
+    expect(getType(9)).toEqual('number');
+    expect(getType(true)).toEqual('boolean');
+    expect(getType([])).toEqual('object');
+    expect(getType({})).toEqual('object');
+    expect(getType(NaN)).toEqual('number');
+    expect(getType(null)).toEqual('object');
+    expect(getType(Object)).toEqual('function');
+  });
+});
+
+describe('getNthElement', () => {
+  const array = ['cat', 'dog', 'elephant', 'fox'];
+
+  it('returns the element at the given position', () => {
+    expect(getNthElement(0, array)).toEqual('cat');
+    expect(getNthElement(2, array)).toEqual('elephant');
+    expect(getNthElement(3, array)).toEqual('fox');
+  });
+
+  it('if n is greater than the number of elements, it cycles back to the start', () => {
+    expect(getNthElement(4, array)).toEqual('cat');
+    expect(getNthElement(5, array)).toEqual('dog');
+  });
+});
+
+describe('arrayToCSVString', () => {
+  it('returns the array elements as a comma-seperated string', () => {
+    expect(arrayToCSVString(['a', 'b', 'c', 'd'])).toEqual('a,b,c,d');
+    expect(arrayToCSVString([1, 2, 3, 4, 5])).toEqual('1,2,3,4,5');
+  });
+});
+
+describe('csvStringToArray', () => {
+  it('converts the csv string as an array', () => {
+    expect(csvStringToArray('a,b,c,d')).toEqual(['a', 'b', 'c', 'd']);
+    expect(csvStringToArray('1,2,3,4,5')).toEqual(['1', '2', '3', '4', '5']);
+  });
+});
+
+describe('addToArray', () => {
+  it('adds the item to the end of the array', () => {
+    const array = [];
+    const array2 = [1, 2, 3];
+
+    expect(addToArray('a', array)).toEqual(undefined);
+    expect(addToArray(4, array2)).toEqual(undefined);
+
+    expect(array).toEqual(['a']);
+    expect(array2).toEqual([1, 2, 3, 4]);
+  });
+});
+
+describe('addToArray2', () => {
+  it('returns a new array with the value appended', () => {
+    const array = ['a', 'b', 'c'];
+    const array2 = [1, 2, 3];
+
+    expect(addToArray2('d', array)).toEqual(['a', 'b', 'c', 'd']);
+    expect(array).toEqual(['a', 'b', 'c']);
+
+    expect(addToArray2(4, array2)).toEqual([1, 2, 3, 4]);
+    expect(array2).toEqual([1, 2, 3]);
+  });
+});
+
+describe('removeNthElement', () => {
+  it('removes the element at position n', () => {
+    const array = ['ant', 'bison', 'cockerel', 'duck', 'elephant'];
+    removeNthElement(2, array);
+    expect(array).toEqual(['ant', 'bison', 'duck', 'elephant']);
+  });
+});
+
+describe('numbersToStrings', () => {
+  it('converts every number in the array to a string', () => {
+    expect(numbersToStrings([1, 2, 3])).toEqual(['1', '2', '3']);
+  });
+});
+
+describe('uppercaseWordsInArray', () => {
+  it('makes every string in the array uppercase', () => {
+    expect(uppercaseWordsInArray(['cat', 'mouse', 'banana'])).toEqual(['CAT', 'MOUSE', 'BANANA']);
+  });
+});
+
+describe('reverseWordsInArray', () => {
+  it('reverses every string in an array', () => {
+    expect(reverseWordsInArray(['cat', 'Mouse', 'banana'])).toEqual(['tac', 'esuoM', 'ananab']);
+  });
+});
+
+describe('onlyEven', () => {
+  it('filters the array and only returns even numbers', () => {
+    expect(onlyEven([1, 2, 3, 4, 5, 6, 7, 8])).toEqual([2, 4, 6, 8]);
+  });
+});
+
+describe('removeNthElement2', () => {
+  it('returns an array with the nth element removed, and does not mutate the original', () => {
+    const array = ['bike', 'car', 'train', 'bus'];
+    expect(removeNthElement2(2, array)).toEqual(['bike', 'car', 'bus']);
+    expect(array).toEqual(['bike', 'car', 'train', 'bus']);
+  });
+});
+
+describe('elementsStartingWithAVowel', () => {
+  it('returns elements starting with a vowel', () => {
+    expect(elementsStartingWithAVowel([
+      'apple', 'bapple', 'capple', 'dapple', 'epple', 'fepple',
+      'gepple', 'hepple', 'ipple', 'jipple', 'kipple', 'lipple',
+      'mipple', 'nipple', 'opple', 'popple', 'qopple', 'ropple',
+      'sopple', 'topple', 'upple', 'vupple', 'wupple', 'xupple',
+      'yupple', 'zupple',
+    ])).toEqual(['apple', 'epple', 'ipple', 'opple', 'upple']);
+  });
+
+  it('is case insensitive', () => {
+    expect(elementsStartingWithAVowel([
+      'Apple', 'Bapple', 'Capple', 'Dapple', 'Epple', 'Fepple',
+      'Gepple', 'Hepple', 'Ipple', 'Jipple', 'Kipple', 'Lipple',
+      'Mipple', 'Nipple', 'Opple', 'Popple', 'Qopple', 'Ropple',
+      'Sopple', 'Topple', 'Upple', 'Vupple', 'Wupple', 'Xupple',
+      'Yupple', 'Zupple',
+    ])).toEqual(['Apple', 'Epple', 'Ipple', 'Opple', 'Upple']);
+  });
+});
+
+describe('removeSpaces', () => {
+  it('returns the string with the space characters removed', () => {
+    expect(removeSpaces('this string has spaces')).toEqual('thisstringhasspaces');
+    expect(removeSpaces(' this one has sneaky   spaces ')).toEqual('thisonehassneakyspaces');
+  });
+});
+
+describe('sumNumbers', () => {
+  it('returns the sum of the numbers in the array', () => {
+    expect(sumNumbers([1, 3, 5, 6, 2, 8])).toEqual(25);
+  });
+});
+
+describe('sortByLastLetter', () => {
+  it('sorts the string by the last character', () => {
+    expect(sortByLastLetter(['Lannister', 'Stark', 'Greyjoy', 'Targaryen']))
+      .toEqual(['Stark', 'Targaryen', 'Lannister', 'Greyjoy']);
+  });
+});
+
+describe('createPerson', () => {
+  it('creates an object with the given name and age properties', () => {
+    expect(createPerson('Fred', 79)).toEqual({
+      name: 'Fred',
+      age: 79,
+    });
+
+    expect(createPerson('Dilys', 50)).toEqual({
+      name: 'Dilys',
+      age: 50,
+    });
+  });
+});
+
+describe('getName', () => {
+  it('returns the name property of the object', () => {
+    expect(getName({
+      name: 'Fred',
+      age: 79,
+    })).toEqual('Fred');
+  });
+});
+
+describe('getProperty', () => {
+  it('returns the given property', () => {
+    expect(getProperty('age', {
+      name: 'Fred',
+      age: 79,
+    })).toEqual(79);
+  });
+});
+
+describe('hasProperty', () => {
+  const fred = {
+    name: 'Fred',
+    age: 79,
+  };
+
+  expect(hasProperty('age', fred)).toBe(true);
+  expect(hasProperty('favouriteColour', fred)).toBe(false);
+});
+
+describe('isOver65', () => {
+  it('returns true if the person is aged over 65', () => {
+    const jim = {
+      name: 'Jim',
+      age: 66,
+    };
+
+    const dilys = {
+      name: 'Dilys',
+      age: 50,
+    };
+
+    const marjorie = {
+      name: 'Marjorie',
+      age: 65,
+    };
+
+    expect(isOver65(jim)).toBe(true);
+    expect(isOver65(dilys)).toBe(false);
+    expect(isOver65(marjorie)).toBe(false);
+  });
+});
+
+describe('getAges', () => {
+  it('returns the ages of each person in the array', () => {
+    const jim = {
+      name: 'Jim',
+      age: 66,
+    };
+
+    const dilys = {
+      name: 'Dilys',
+      age: 50,
+    };
+
+    const marjorie = {
+      name: 'Marjorie',
+      age: 65,
+    };
+
+    expect(getAges([jim, dilys, marjorie])).toEqual([66, 50, 65]);
+  });
+});
+
+describe('findByName', () => {
+  it('returns the person with the given name', () => {
+    const jim = {
+      name: 'Jim',
+      age: 66,
+    };
+
+    const dilys = {
+      name: 'Dilys',
+      age: 50,
+    };
+
+    const marjorie = {
+      name: 'Marjorie',
+      age: 65,
+    };
+
+    expect(findByName('Jim', [jim, dilys, marjorie])).toBe(jim);
+    expect(findByName('Marjorie', [jim, dilys, marjorie])).toBe(marjorie);
+  });
+});
+
+describe('findHondas', () => {
+  it('returns a list of cars manufactured by Honda', () => {
+    const car1 = {
+      manufacturer: 'Honda',
+      year: 1997,
+      colour: 'blue',
+    };
+
+    const car2 = {
+      manufacturer: 'Fiat',
+      year: 2010,
+      colour: 'green',
+    };
+
+    const car3 = {
+      manufacturer: 'Toyota',
+      year: 2017,
+      colour: 'blue',
+    };
+
+    const car4 = {
+      manufacturer: 'Honda',
+      year: 2001,
+      colour: 'red',
+    };
+
+    expect(findHondas([car1, car2, car3, car4])).toEqual([car1, car4]);
+  });
+});
+
+describe('averageAge', () => {
+  it('returns the average age of the people in the list', () => {
+    const john = {
+      name: 'John',
+      age: 60,
+    };
+
+    const eric = {
+      name: 'Eric',
+      age: 50,
+    };
+
+    const gary = {
+      name: 'Gary',
+      age: 25,
+    };
+
+    expect(averageAge([john, eric, gary])).toBe(45);
+  });
+});
+
+describe('createTalkingPerson', () => {
+  it('returns a person who can introduce themselves', () => {
+    const bill = createTalkingPerson('Bill', 40);
+    expect(bill).toEqual({
+      name: 'Bill',
+      age: 40,
+      introduce: expect.any(Function),
+    });
+
+    expect(bill.introduce('Fred')).toEqual('Hi Fred, my name is Bill and I am 40!');
+  });
+});
