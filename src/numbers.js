905b9caa18eb68934a40f7bb7cfc3ebb0a3aafa5
Clover Chapman <clo@cloverseeds.co.uk>
2018-02-14 13:40:53 +0000

    numbers and strings passing tests

--- src/numbers.js
+++ src/numbers.js
@@ -1,43 +1,43 @@
 export const add = (a, b) => {
-  // your code here
+  return a + b;
 };
 
 export const subtract = (a, b) => {
-  // your code here
+  return a - b;
 };
 
 export const multiply = (a, b) => {
-  // your code here
+  return a * b;
 };
 
 export const divide = (a, b) => {
-  // your code here
+  return a / b;
 };
 
 export const power = (a, b) => {
-  // your code here
+  return a ** b;
 };
 
 export const round = (a) => {
-  // your code here
+  return Math.round(a);
 };
 
 export const roundUp = (a) => {
-  // your code here
+  return Math.ceil(a);
 };
 
 export const roundDown = (a) => {
-  // your code here
+  return Math.floor(a);
 };
 
 export const absolute = (a) => {
-  // your code here
+  return Math.abs(a);
 };
 
 export const quotient = (a, b) => {
-  // your code here
+  return parseInt(a/b);
 };
 
 export const remainder = (a, b) => {
-  // your code here
+  return a % b;
 };
