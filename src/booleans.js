cbf0fbd21080b69d3abb79464b3238af4661ebfb
SharifCoding <macbook@Apples-MacBook-Pro.local>
2018-02-14 16:44:31 +0000

    numbers.js and strings.js completed

--- src/booleans.js
+++ src/booleans.js
@@ -1,59 +1,97 @@
 export const negate = (a) => {
-  // your code here
+    if (a == true) {
+    return false;
+    } else {
+    return true;
+    }
+    // returns the opposite of the passed boolean value
+    // expect(negate(true)).toBe(false)
 };
 
 export const both = (a, b) => {
-  // your code here
+    if (a && b == true) {
+    // logical AND
+    return true;
+    } else {
+    return false;
+    }
+    // returns true if both of the given values are true
+    // expect(both(true, true)).toBe(true)
 };
 
 export const either = (a, b) => {
-  // your code here
+    if (a || b == true) {
+    // logical OR
+    return true;
+    } else {
+    return false;
+    }
+    // returns true if at least one of the given values are true
+    // expect(either(true, true)).toBe(true)
 };
 
 export const none = (a, b) => {
-  // your code here
+    if (a || b == true) {
+    // logical NOT
+    return false;
+    } else {
+    return true;
+    }
+    // returns true if neither of the given values are true
+    // expect(none(true, true)).toBe(false)
 };
 
 export const one = (a, b) => {
-  // your code here
+    // returns true if exactly one of the given values are true
+    // expect(one(true, true)).toBe(false)
 };
 
 export const truthiness = (a) => {
-  // your code here
+    // returns the truthiness of the given value
+    // expect(truthiness('')).toBe(false)
 };
 
 export const isEqual = (a, b) => {
-  // your code here
+    // returns whether the two values are even
+    // expect(isEqual(true, false)).toBe(false)
 };
 
 export const isGreaterThan = (a, b) => {
-  // your code here
+    // returns true if the first number is strictly greater than the second
+    // expect(isGreaterThan(1, 2)).toBe(false)
 };
 
 export const isLessThanOrEqualTo = (a, b) => {
-  // your code here
+    // returns true if the first number is less than or equal to the second
+    // expect(isLessThanOrEqualTo(1, 2)).toBe(true)
 };
 
 export const isOdd = (a) => {
-  // your code here
+    // returns whether the number is odd
+    // expect(isOdd(5)).toBe(true)
 };
 
 export const isEven = (a) => {
-  // your code here
+    // returns whether the number is even
+    // expect(isEven(5)).toBe(false)
 };
 
 export const isSquare = (a) => {
-  // your code here
+    // returns true if the number is a square
+    // expect(isSquare(9)).toEqual(true)
 };
 
 export const startsWith = (char, string) => {
-  // your code here
+    // returns whether the given string starts with the given character
+    // expect(startsWith('a', 'aardvark')).toBe(true)
 };
 
 export const containsVowels = (string) => {
-  // your code here
+    // returns whether the given string contains vowels
+    // expect(containsVowels('cat')).toBe(true)
 };
 
 export const isLowerCase = (string) => {
-  // your code here
+    // it returns true if the given string is lowercase
+    // expect(isLowerCase('abc')).toBe(true)
 };
--- src/numbers.js
+++ src/numbers.js
@@ -1,43 +1,65 @@
 export const add = (a, b) => {
-  // your code here
+    return a + b;
+    // adds the two numbers together
+    // expect(add(2, 1)).toEqual(3)
 };
 
 export const subtract = (a, b) => {
-  // your code here
+    return a - b;
+    // subtracts the second number from the first
+    // expect(subtract(2, 1)).toEqual(1)
 };
 
 export const multiply = (a, b) => {
-  // your code here
+    return a * b;
+    // multiplies the two numbers together
+    // expect(multiply(10, 3)).toEqual(30)
 };
 
 export const divide = (a, b) => {
-  // your code here
+    return a / b;
+    // divides the first number by the second number
+    // expect(divide(20, 5)).toEqual(4)
 };
 
 export const power = (a, b) => {
-  // your code here
+    return Math.pow(a, b);
+    // returns the first number to the power of the second
+    // expect(power(5, 2)).toEqual(25)
 };
 
 export const round = (a) => {
-  // your code here
+    return Math.round(a);
+    // rounds the number to the nearest integer
+    // expect(round(2.1)).toEqual(2)
 };
 
 export const roundUp = (a) => {
-  // your code here
+    return Math.ceil(a);
+    // rounds the number up to the nearest integer
+    // expect(roundUp(2.1)).toEqual(3)
 };
 
 export const roundDown = (a) => {
-  // your code here
+    return Math.floor(a);
+    // rounds the number down to the nearest integer
+    // expect(roundDown(2.1)).toEqual(2)
 };
 
 export const absolute = (a) => {
-  // your code here
+    return Math.abs(a);
+    // returns the absolute value of the number
+    // expect(absolute(-1)).toEqual(1)
 };
 
 export const quotient = (a, b) => {
-  // your code here
+    return parseInt(a / b);
+    // returns the quotient from dividing the first number by the second number
+    // expect(quotient(10, 3)).toEqual(3)
 };
 
 export const remainder = (a, b) => {
-  // your code here
+    return a % b;
+    // returns the remainder when dividing the first number by the second number
+    // expect(remainder(10, 3)).toEqual(1)
 };
