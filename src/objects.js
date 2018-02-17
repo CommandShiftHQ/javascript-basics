e80ce8dde2510788e5e859afbb371146c5e65747
David Sheffield <davesheff@live.co.uk>
2018-02-17 21:26:05 +0000

    objects problems - 8 completed

--- src/objects.js
+++ src/objects.js
@@ -20,18 +20,39 @@ export const isOver65 = (person) => {
 };
 
 export const getAges = (people) => {
+  let agesArray = []
+  for(var i=0; i<people.length; i++) {
+    agesArray[i] = people[i].age}
+    return agesArray
   // your code here
 };
 
 export const findByName = (name, people) => {
+  for(var i= 0, l = people.length; i< l; i++){
+    if(people[i].name === name ){
+    return people[i]
+    }
+  }
   // your code here
 };
 
 export const findHondas = (cars) => {
-  // your code here
+  let newArr = []
+  for(let i= 0; i <= cars.length; i++){
+   if (cars[i].manufacturer === 'Honda'){
+   newArr.push(cars[i])
+   }
+  return newArr
+  }// your code here
 };
 
 export const averageAge = (people) => {
+  let newArr = []
+  for(var i= 0; i < people.length; i++){
+   newArr.push(people[i].age)
+   }
+   let total = newArr.reduce((total, next) => total + next)
+   return total / newArr.length
   // your code here
 };
 
