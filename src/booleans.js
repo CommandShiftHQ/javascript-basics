5ef9dccd636869c0cdc575ad1e819c798d489c3b
Reetu Child <rdhir79@hotmail.co.uk>
2018-02-17 14:41:18 +0000

    added more booleans code

--- src/booleans.js
+++ src/booleans.js
@@ -41,43 +41,132 @@ else {
 };
 
 export const one = (a, b) => {
-  // your code here
+  if(a && b || !a && !b ) {
+  
+  return false 
+}
+else {
+	return true
+}
 };
 
 export const truthiness = (a) => {
-  // your code here
+  if(a) {
+  
+  return true
+ 
+}
+
+else {
+
+  return false
+}
 };
 
 export const isEqual = (a, b) => {
-  // your code here
+   if(a === b) {
+  
+  return true
+ 
+}
+
+else {
+
+  return false
+}
 };
 
 export const isGreaterThan = (a, b) => {
-  // your code here
+   if(a > b) {
+  
+  return true
+ 
+}
+
+else {
+
+  return false
+}
 };
 
 export const isLessThanOrEqualTo = (a, b) => {
-  // your code here
+   if(a <= b) {
+  
+  return true
+ 
+}
+
+else {
+
+  return false
+}
 };
 
 export const isOdd = (a) => {
-  // your code here
+   if(a % 2 !== 0) {
+  
+  return true
+ 
+}
+
+else {
+
+  return false
+}
 };
 
 export const isEven = (a) => {
-  // your code here
+  if(a % 2 === 0) {
+  
+  return true
+ 
+}
+
+else {
+
+  return false
+}
 };
 
 export const isSquare = (a) => {
-  // your code here
+
+  if (a >= 0 && Math.sqrt(a) % 1 === 0) {
+	
+  return true
+  }
+
+else {
+
+  return false
+}
 };
 
 export const startsWith = (char, string) => {
-  // your code here
+  if (string[0] === char) {
+	
+  return true
+  }
+
+else {
+
+  return false
+}
 };
 
 export const containsVowels = (string) => {
-  // your code here
+  for (i=0; i < string.length; i++) {
+  
+  if(string[i] === "a" || string[i] === "e" || string[i] === "i" || string[i] === "o"
+    || string[i] === "u" || string[i] === "A" || string[i] === "E" || string[i] === "I" || string[i] === "O"
+    || string[i] === "U") {
+      
+     return true
+    
+    break     
+  }
+else {
+return false
+}
 };
 
 export const isLowerCase = (string) => {
