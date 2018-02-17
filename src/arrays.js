37ddd97061176800c29add32662905322f25bd40
SharifCoding <macbook@Apples-MacBook-Pro.local>
2018-02-17 20:58:26 +0000

    updated arrays.js

--- src/arrays.js
+++ src/arrays.js
@@ -1,59 +1,159 @@
 export const getNthElement = (index, array) => {
-  // your code here
+    if (index > array.length -1){
+        return array[index - array.length];
+    }
+    return array[index];
 };
+/*
+const array = ['cat', 'dog', 'elephant', 'fox']
+
+'returns the element at the given position'
+expect(getNthElement(0, array)).toEqual('cat')
+expect(getNthElement(2, array)).toEqual('elephant')
+expect(getNthElement(3, array)).toEqual('fox')
+
+'if n is greater than the number of elements, it cycles back to the start'
+expect(getNthElement(4, array)).toEqual('cat')
+expect(getNthElement(5, array)).toEqual('dog')
+*/
 
 export const arrayToCSVString = (array) => {
-  // your code here
+    return array.join();
 };
+/*
+'returns the array elements as a comma-seperated string'
+expect(arrayToCSVString(['a', 'b', 'c', 'd'])).toEqual('a,b,c,d')
+expect(arrayToCSVString([1, 2, 3, 4, 5])).toEqual('1,2,3,4,5')
+*/
 
 export const csvStringToArray = (string) => {
-  // your code here
+    return string.split(",");
 };
+/*
+'converts the csv string as an array'
+expect(csvStringToArray('a,b,c,d')).toEqual(['a', 'b', 'c', 'd'])
+expect(csvStringToArray('1,2,3,4,5')).toEqual(['1', '2', '3', '4', '5'])
+*/
 
-export const addToArray = (array, element) => {
-  // your code here
+export const addToArray = (element, array) => {
+    array.push(element);
 };
+/*
+'adds the item to the end of the array'
+const array = []
+const array2 = [1, 2, 3]
+    
+expect(addToArray('a', array)).toEqual(undefined)
+expect(addToArray(4, array2)).toEqual(undefined)
 
-export const addToArray2 = (array, element) => {
-  // your code here
+expect(array).toEqual(['a'])
+expect(array2).toEqual([1, 2, 3, 4])
+*/
+
+export const addToArray2 = (element, array) => {
+    return array.concat(element);
 };
+/*
+'returns a new array with the value appended'
+const array = ['a', 'b', 'c'];
+const array2 = [1, 2, 3];
+
+expect(addToArray2('d', array)).toEqual(['a', 'b', 'c', 'd'])
+expect(array).toEqual(['a', 'b', 'c'])
+
+expect(addToArray2(4, array2)).toEqual([1, 2, 3, 4])
+expect(array2).toEqual([1, 2, 3])
+*/
 
 export const removeNthElement = (index, array) => {
-  // your code here
+    return array.splice(index, 1);
+    // array.splice(index, howmany)
+    // https://blog.mariusschulz.com/2016/07/16/removing-elements-from-javascript-arrays
 };
+/*
+'removes the element at position n'
+const array = ['ant', 'bison', 'cockerel', 'duck', 'elephant']
+removeNthElement(2, array)
+expect(array).toEqual(['ant', 'bison', 'duck', 'elephant'])
+*/
 
 export const numbersToStrings = (numbers) => {
-  // your code here
+    return numbers.map(String);
 };
+/*
+'converts every number in the array to a string'
+expect(numbersToStrings([1, 2, 3])).toEqual(['1', '2', '3'])
+*/
 
 export const uppercaseWordsInArray = (strings) => {
-  // your code here
+    return strings.map(function(n){
+        return(n).toUpperCase()});
 };
+/*
+'makes every string in the array uppercase'
+expect(uppercaseWordsInArray(['cat', 'mouse', 'banana'])).toEqual(['CAT', 'MOUSE', 'BANANA'])
+*/
 
 export const reverseWordsInArray = (strings) => {
-  // your code here
+    return strings.split("").reverse().join("");
+    // http://eddmann.com/posts/ten-ways-to-reverse-a-string-in-javascript/
 };
+/*
+'reverses every string in an array'
+expect(reverseWordsInArray(['cat', 'Mouse', 'banana'])).toEqual(['tac', 'esuoM', 'ananab'])
+*/
 
 export const onlyEven = (numbers) => {
-  // your code here
+    return numbers.filter(function(n){return n % 2===0})
+    // https://gist.github.com/bondarewicz/8843037
 };
+/*
+'filters the array and only returns even numbers'
+expect(onlyEven([1, 2, 3, 4, 5, 6, 7, 8])).toEqual([2, 4, 6, 8])
+*/
 
 export const removeNthElement2 = (index, array) => {
-  // your code here
+    return array.slice(index);
 };
+/*
+'returns an array with the nth element removed, and does not mutate the original'
+const array = ['bike', 'car', 'train', 'bus'];
+expect(removeNthElement2(2, array)).toEqual(['bike', 'car', 'bus'])
+expect(array).toEqual(['bike', 'car', 'train', 'bus'])
+*/
 
 export const elementsStartingWithAVowel = (strings) => {
   // your code here
 };
+/*
+'returns elements starting with a vowel'
+expect(elementsStartingWithAVowel(['apple', 'bapple', 'capple', 'dapple', 'epple', 'fepple', 'gepple', 'hepple', 'ipple', 'jipple', 'kipple', 'lipple', 'mipple', 'nipple', 'opple', 'popple', 'qopple', 'ropple', 'sopple', 'topple', 'upple', 'vupple', 'wupple', 'xupple', 'yupple', 'zupple',])).toEqual(['apple', 'epple', 'ipple', 'opple', 'upple'])
+
+'is case insensitive'
+expect(elementsStartingWithAVowel(['Apple', 'Bapple', 'Capple', 'Dapple', 'Epple', 'Fepple', 'Gepple', 'Hepple', 'Ipple', 'Jipple', 'Kipple', 'Lipple', 'Mipple', 'Nipple', 'Opple', 'Popple', 'Qopple', 'Ropple', 'Sopple', 'Topple', 'Upple', 'Vupple', 'Wupple', 'Xupple', 'Yupple', 'Zupple'])).toEqual(['Apple', 'Epple', 'Ipple', 'Opple', 'Upple'])
+*/
 
 export const removeSpaces = (string) => {
   // your code here
 };
+/*
+'returns the string with the space characters removed'
+expect(removeSpaces('this string has spaces')).toEqual('thisstringhasspaces')
+expect(removeSpaces(' this one has sneaky   spaces ')).toEqual('thisonehassneakyspaces')
+*/
 
 export const sumNumbers = (numbers) => {
   // your code here
 };
+/*
+'returns the sum of the numbers in the array'
+expect(sumNumbers([1, 3, 5, 6, 2, 8])).toEqual(25)
+*/
 
 export const sortByLastLetter = (strings) => {
   // your code here
 };
+/*
+'sorts the string by the last character'
+expect(sortByLastLetter(['Lannister', 'Stark', 'Greyjoy', 'Targaryen'])).toEqual(['Stark', 'Targaryen', 'Lannister', 'Greyjoy'])
+*/
