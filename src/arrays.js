481e5e33e9fd1e96451e3e96fee5a637aa25850d
Clover Chapman <clo@cloverseeds.co.uk>
2018-02-17 19:03:10 +0000

    arrays - over half way there

--- src/arrays.js
+++ src/arrays.js
@@ -1,41 +1,64 @@
 export const getNthElement = (index, array) => {
-  // your code here
+  //if n is greater than the number of elements, it cycles back to the start
+    if (index > array.length - 1) {
+      index = (index - array.length)
+    }
+      return (array[index]);
 };
 
 export const arrayToCSVString = (array) => {
-  // your code here
+  // returns the array elements as a comma-seperated string (defalt seperator is comma)
+  return array.join();
 };
 
 export const csvStringToArray = (string) => {
-  // your code here
+  // converts the csv string as an array
+  return string.split(',');
 };
 
-export const addToArray = (array, element) => {
-  // your code here
+export const addToArray = (element, array) => {
+  // adds the item to the end of the array
+  return array.push(element);
 };
 
-export const addToArray2 = (array, element) => {
-  // your code here
+export const addToArray2 = (element, array) => {
+  // returns a new array with the value appended
+  return array.concat(element);
 };
 
 export const removeNthElement = (index, array) => {
-  // your code here
+  // removes the element at position n
+  return array.splice(index, 1);
 };
 
 export const numbersToStrings = (numbers) => {
-  // your code here
+  // converts every number in the array to a string
+  // use map and pass the String constructor as a function, which will turn each number into a string
+  return numbers.map(String);  
+
 };
 
 export const uppercaseWordsInArray = (strings) => {
-  // your code here
+  
+  strings = strings.map(v => v.toUpperCase());
+
+  return strings;
 };
 
 export const reverseWordsInArray = (strings) => {
-  // your code here
+  // reverses every string in an array
+  strings = arrayToCSVString (strings);
+ 
+  strings = strings.split("").reverse().join("");
+
+  strings = csvStringToArray (strings);
+  
+  return strings.reverse();
 };
 
 export const onlyEven = (numbers) => {
-  // your code here
+  //filters the array and only returns even numbers
+
 };
 
 export const removeNthElement2 = (index, array) => {
