17e8931a3793619d6c83e5a05e43f6acbabce16a
David Sheffield <davesheff@live.co.uk>
2018-02-17 17:41:10 +0000

    objects problems - partially completed

--- src/objects.js
+++ src/objects.js
@@ -1,21 +1,22 @@
 export const createPerson = (name, age) => {
-  // your code here
+  let person = {name: name, age: age};
+  return person// your code here
 };
 
 export const getName = (object) => {
-  // your code here
+  return object.name// your code here
 };
 
 export const getProperty = (property, object) => {
-  // your code here
+  return object[property]// your code here
 };
 
 export const hasProperty = (property, object) => {
-  // your code here
+  return object.hasOwnProperty(property)// your code here
 };
 
 export const isOver65 = (person) => {
-  // your code here
+  if (person.age > 65) {return true} else return false// your code here
 };
 
 export const getAges = (people) => {
