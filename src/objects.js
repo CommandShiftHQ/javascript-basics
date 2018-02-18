ec380ecd6fe90ddc36f58e468e3d12bb8a712b84
Clover Chapman <clo@cloverseeds.co.uk>
2018-02-18 23:14:49 +0000

    passing 6 tests

--- src/objects.js
+++ src/objects.js
@@ -1,29 +1,46 @@
 export const createPerson = (name, age) => {
-  // your code here
+  // creates an object with the given name and age properties
+  let person = {
+    name: name,
+    age: age
+  };
+
+  return (person);
+
 };
 
 export const getName = (object) => {
-  // your code here
+  // returns the name property of the object
+  return(object["name"]);
+
 };
 
 export const getProperty = (property, object) => {
-  // your code here
+  return(object[property]);
 };
 
 export const hasProperty = (property, object) => {
-  // your code here
+  // checks whether object has given property
+  return object.hasOwnProperty(property);
 };
 
 export const isOver65 = (person) => {
-  // your code here
+  // returns true if the person is aged over 65
+  if (person['age'] > 65) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 export const getAges = (people) => {
-  // your code here
+  // returns the ages of each person in the array
+  //return people.filter()
 };
 
 export const findByName = (name, people) => {
   // your code here
+  return people.find(person => person.name === name);
 };
 
 export const findHondas = (cars) => {
