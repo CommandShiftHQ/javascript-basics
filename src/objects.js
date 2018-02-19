f5340094dc2161f982f7c2f93106316cc8ed5de6
Reetu Child <rdhir79@hotmail.co.uk>
2018-02-19 21:06:07 +0000

    add objects files - last one not completed

--- src/objects.js
+++ src/objects.js
@@ -1,7 +1,8 @@
 export const createPerson = (name, age) => {
   return {
     name : name,
-    age : age
+    age : age,
+    
 }
 };
 
@@ -33,21 +34,62 @@ return false
 };
 
 export const getAges = (people) => {
-  // your code here
+let agesArr = []  
+for (let i = 0; i < people.length; i++ )
+ {
+
+	agesArr.push(people[i].age)
+
+ }
+
+	return agesArr
 };
 
 export const findByName = (name, people) => {
-  // your code here
+  
+for (let i = 0; i < people.length; i++ )
+ {
+   if(people[i].name === name) {
+
+	return people[i]
+	break
+ 
+   }
+ }
 };
 
 export const findHondas = (cars) => {
-  // your code here
+  let carsArr = []
+
+for (let i = 0; i < cars.length; i++ )
+ {
+   if(cars[i].manufacturer === 'Honda') {
+
+	carsArr.push(cars[i])
+ 
+   }
+ }
+
+return carsArr
 };
 
 export const averageAge = (people) => {
-  // your code here
+
+let agesArr = []
+	for (let i = 0; i < people.length; i++ )
+ {
+   agesArr.push(people[i].age)
+ 
+   }
+let averageAge = 0
+
+    averageAge += agesArr.reduce((a, b) => a + b)
+    / agesArr.length;
+
+return averageAge
+
 };
 
 export const createTalkingPerson = (name, age) => {
-  // your code here
+  return `Hi Fred, my name is $name and I am $age!` 
 };
