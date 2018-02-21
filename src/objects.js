9923d8bd9e024e34b0b906d4b4732224dcad47db
Joe Allen <archerpark@hotmail.com>
2018-02-21 09:35:53 +0000

    Objects solved

--- src/objects.js
+++ src/objects.js
@@ -1,39 +1,82 @@
 export const createPerson = (name, age) => {
-  // your code here
+  let person = {
+  name: name,
+  age: age
+  }
+  return person;
 };
 
 export const getName = (object) => {
-  // your code here
+  return object.name;
 };
 
 export const getProperty = (property, object) => {
-  // your code here
+  return object.age;
 };
 
 export const hasProperty = (property, object) => {
-  // your code here
+  if (object.hasOwnProperty(property)){
+    return true; 
+  }else{
+    return false;
+  }
 };
 
 export const isOver65 = (person) => {
-  // your code here
+  if (person.age > 65){
+    return true;
+  }else{
+    return false;
+  }
 };
 
 export const getAges = (people) => {
-  // your code here
+  let peopleAge = [];
+  for (let i = 0; i< people.length; i++){
+    peopleAge.push(people[i].age);
+  }
+  return peopleAge;
 };
 
 export const findByName = (name, people) => {
-  // your code here
+
+   let found = people.findIndex((person) => {
+     return person.name === name;
+  });
+  return people[found];
 };
 
 export const findHondas = (cars) => {
-  // your code here
+  let carsArray = []; 
+  for (let i = 0; i<cars.length; i++){
+    let carValues = Object.values(cars[i]);
+    for (let j=0; j<3; j++){ 
+      if (carValues[j] === "Honda"){
+       carsArray.push(cars[i]);
+    }
+  }
+
+  }
+  return carsArray;
+  
 };
 
 export const averageAge = (people) => {
-  // your code here
+  let ages = [];
+  for (let i=0; i<people.length; i++){
+    ages.push(people[i].age);
+  }
+  let average = (ages[0] + ages[1] + ages[2])/3;
+  return average; 
 };
 
 export const createTalkingPerson = (name, age) => {
-  // your code here
+  const  person = {
+    name: name,
+    age: age,
+    introduce: function(friend){
+      return("Hi " + friend + ", my name is " + name + " and I am " + age + "!");
+    }
+  }
+  return person;
 };
