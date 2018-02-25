9384a222744b3c9ebcd073f5fa65ca2c8d97d71f
Reetu Child <rdhir79@hotmail.co.uk>
2018-02-25 21:05:10 +0000

    added strings file

--- src/strings.js
+++ src/strings.js
@@ -1,23 +1,23 @@
 export const sayHello = (string) => {
-  // your code here
+	return `Hello, ${string}!`;
 };
 
 export const uppercase = (string) => {
-  // your code here
+	return string.toUpperCase();
 };
 
 export const lowercase = (string) => {
-  // your code here
+	return string.toLowerCase()
 };
 
 export const countCharacters = (string) => {
-  // your code here
+ 	return string.length
 };
 
 export const firstCharacter = (string) => {
-  // your code here
+	return string[0]
 };
 
 export const firstCharacters = (string, n) => {
-  // your code here
+	return string.substr(0, n)
 };
