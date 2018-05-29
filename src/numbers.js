f996173852c6d9a49c5eb72648b3f5746f4e64bd
BenMasonG <BenMasonG@protonmail.com>
2018-05-29 22:31:28 +0100

    stage 2, numbers completed

--- src/numbers.js
+++ src/numbers.js
@@ -1,57 +1,57 @@
 const add = (a, b) => {
-  // your code here
+    return a + b; // your code here
 };
 
 const subtract = (a, b) => {
-  // your code here
+    return a - b; // your code here
 };
 
 const multiply = (a, b) => {
-  // your code here
+    return a * b; // your code here
 };
 
 const divide = (a, b) => {
-  // your code here
+    return a / b; // your code here
 };
 
 const power = (a, b) => {
-  // your code here
+    return a ** b; // your code here
 };
 
 const round = (a) => {
-  // your code here
+    return Math.round(a); //  your code here
 };
 
 const roundUp = (a) => {
-  // your code here
+    return Math.ceil(a); // your code here
 };
 
 const roundDown = (a) => {
-  // your code here
+    return Math.floor(a); // your code here
 };
 
 const absolute = (a) => {
-  // your code here
+    return Math.abs(a); // your code here
 };
 
 const quotient = (a, b) => {
-  // your code here
-};
+    return parseInt(a / b);
+}
 
 const remainder = (a, b) => {
-  // your code here
+    return a % b; // your code here
 };
 
 module.exports = {
-  add,
-  subtract,
-  multiply,
-  divide,
-  power,
-  round,
-  roundUp,
-  roundDown,
-  absolute,
-  quotient,
-  remainder
+    add,
+    subtract,
+    multiply,
+    divide,
+    power,
+    round,
+    roundUp,
+    roundDown,
+    absolute,
+    quotient,
+    remainder
 }
