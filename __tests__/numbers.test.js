a7ebeb2ecc31197bb3969fcbc1f660f0749688e2
Michael Harrison <harrim91@hotmail.co.uk>
2018-08-19 22:33:22 +0100

    Add quotient description

--- __tests__/numbers.test.js
+++ __tests__/numbers.test.js
@@ -89,6 +89,9 @@ describe('absolute', () => {
 });
 
 describe('quotient', () => {
+  // N.B. quotient of two numbers is the integer given by dividing the first by the second, without the remainder
+  // 18 divided by 7 is 2 remainder 4 (or 2.571...)
+  // so the quotient of 18 and 7 is 2
   xit('returns the quotient from dividing the first number by the second number', () => {
     expect(quotient(10, 3)).toEqual(3);
     expect(quotient(18, 7)).toEqual(2);
