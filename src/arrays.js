94ce1bb86b97b9419ca27b03044656ed5dd2d987
Vitnija Bluzma <thepunkyone@gmail.com>
2019-02-11 03:09:08 +0000

    completed arrays test

--- src/arrays.js
+++ src/arrays.js
@@ -1,61 +1,70 @@
 const getNthElement = (index, array) => {
-  // your code here
+  const newIndex = index < array.length ? index : index - array.length;
+  return array[newIndex];
 };
 
 const arrayToCSVString = (array) => {
-  // your code here
+  return array.join();
 };
 
 const csvStringToArray = (string) => {
-  // your code here
+  return string.split(',');
 };
 
 const addToArray = (element, array) => {
-  // your code here
+  array.push(element);
 };
 
 const addToArray2 = (element, array) => {
-  // your code here
+  const newArray = [...array, element];
+  return newArray;
 };
 
 const removeNthElement = (index, array) => {
-  // your code here
+  array.splice(index, 1);
+  return array;
 };
 
 const numbersToStrings = (numbers) => {
-  // your code here
+  return numbers.map(String);
 };
 
 const uppercaseWordsInArray = (strings) => {
-  // your code here
+  return strings.map((item) => item.toUpperCase());
 };
 
 const reverseWordsInArray = (strings) => {
-  // your code here
+  return strings.map((item) => item.split('').reverse().join(''));
 };
 
 const onlyEven = (numbers) => {
-  // your code here
+  return numbers.filter((number) => number % 2 === 0);
 };
 
 const removeNthElement2 = (index, array) => {
-  // your code here
+  return array.filter((_, itemIndex) => itemIndex !== index);
 };
 
 const elementsStartingWithAVowel = (strings) => {
-  // your code here
+  
+  return strings.filter((item) => {
+    const firstLetter = item[0].toLowerCase();
+    return firstLetter === 'a' || firstLetter === 'e' || firstLetter === 'i' || firstLetter === 'o' || firstLetter === 'u';
+  });
 };
 
 const removeSpaces = (string) => {
-  // your code here
+  return string.split(' ').join('');
 };
 
 const sumNumbers = (numbers) => {
-  // your code here
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+  return numbers.reduce(reducer);
 };
 
 const sortByLastLetter = (strings) => {
-  // your code here
+  const reverseString = (item) => item.split('').reverse().join('');
+  return strings.map(reverseString).sort().map(reverseString);
 };
 
 module.exports = {
