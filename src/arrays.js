dc519a0d9fe733489176e9109820603fe46dc4f9
Jack Pearsall <jack.pearsall@visiosound.co.uk>
2019-02-11 22:09:59 +0000

    Solved arrays test

--- src/arrays.js
+++ src/arrays.js
@@ -1,61 +1,141 @@
 const getNthElement = (index, array) => {
-  // your code here
+  const maxArrayindex = (array.length - 1);
+  // console.log(maxArrayindex);
+  const indexGiven = index;
+  // console.log(indexGiven);
+
+  if (indexGiven > maxArrayindex) {
+    const newIndex = indexGiven - 4;
+    // console.log('here');
+    return array[newIndex];
+  }
+  return array[indexGiven];
 };
 
 const arrayToCSVString = (array) => {
-  // your code here
+  const csvArray = array.toString();
+  return csvArray;
 };
 
 const csvStringToArray = (string) => {
-  // your code here
+  const array = string.split(',');
+  return array;
 };
 
 const addToArray = (element, array) => {
-  // your code here
+  array.push(element);
 };
 
 const addToArray2 = (element, array) => {
-  // your code here
+  if (element !== '') {
+    const array2 = array.concat(element);
+    return array2;
+  }
+  return array;
 };
 
 const removeNthElement = (index, array) => {
-  // your code here
+  array.splice(index, 1);
 };
 
 const numbersToStrings = (numbers) => {
-  // your code here
+  const numberStringArray = [];
+  for (let i = 0; i < numbers.length; i++) {
+    numberStringArray.push(numbers[i].toString());
+  }
+  return numberStringArray;
 };
 
 const uppercaseWordsInArray = (strings) => {
-  // your code here
+  const upperCaseArray = [];
+  for (let i = 0; i < strings.length; i++) {
+    upperCaseArray.push(strings[i].toUpperCase());
+  }
+  return upperCaseArray;
 };
 
 const reverseWordsInArray = (strings) => {
-  // your code here
+  const reversedStrings = [];
+  for (let i = 0; i < strings.length; i++) {
+    const currentString = strings[i];
+    let reverseString = '';
+    for (let j = currentString.length - 1; j >= 0; j--) {
+      reverseString += currentString[j];
+    }
+    reversedStrings.push(reverseString);
+  }
+  return reversedStrings;
 };
 
 const onlyEven = (numbers) => {
-  // your code here
+  const evens = [];
+  return numbers.filter((element) => {
+    if (element % 2 !== 0) {
+      return (element[0]);
+    }
+    return evens;
+  });
 };
 
 const removeNthElement2 = (index, array) => {
-  // your code here
+  const newArray = array.slice(0, index).concat(array.slice(index + 1, array.length));
+  return newArray;
 };
 
 const elementsStartingWithAVowel = (strings) => {
-  // your code here
+  // const vowelElements = [];
+  // for (let i = 0; i < strings.length; i++) {
+  //   const currentString = strings[i];
+  //   const lowerString = currentString.toLowerCase();
+  //   if (lowerString[0] === 'a' || lowerString[0] === 'e' || lowerString[0] === 'i' || lowerString[0] === 'o' || lowerString[0] === 'u') {
+  //     vowelElements.push(currentString);
+  //   }
+  // }
+  // return vowelElements;
+
+  return strings.filter((element) => {
+    const lowerString = element.toLowerCase();
+    return lowerString[0] === 'a' || lowerString[0] === 'e' || lowerString[0] === 'i' || lowerString[0] === 'o' || lowerString[0] === 'u';
+  });
 };
 
 const removeSpaces = (string) => {
-  // your code here
+  const noSpaceString = string.replace(/\s/g, '');
+  return noSpaceString;
 };
 
 const sumNumbers = (numbers) => {
-  // your code here
+  let total = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    total += numbers[i];
+  }
+  return total;
 };
 
 const sortByLastLetter = (strings) => {
-  // your code here
+  const reversedStrings = [];
+  for (let i = 0; i < strings.length; i++) {
+    const currentString = strings[i];
+    let reverseString = '';
+    for (let j = currentString.length - 1; j >= 0; j--) {
+      reverseString += currentString[j];
+    }
+    reversedStrings.push(reverseString);
+  } // Reversed the string to get last letter at the start
+
+  reversedStrings.sort();
+
+  const reReversedStrings = [];
+
+  for (let j = 0; j < reversedStrings.length; j++) {
+    const currentString2 = reversedStrings[j];
+    let reverseString2 = '';
+    for (let k = currentString2.length - 1; k >= 0; k--) {
+      reverseString2 += currentString2[k];
+    }
+    reReversedStrings.push(reverseString2);
+  }
+  return reReversedStrings;
 };
 
 module.exports = {
