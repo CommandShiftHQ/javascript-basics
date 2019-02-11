3f0f4166d869ad142275ff7dc2bbbea79f67e228
Jack Pearsall <jack.pearsall@visiosound.co.uk>
2019-02-11 22:16:25 +0000

    Solved 9/10 objects tests

--- src/objects.js
+++ src/objects.js
@@ -1,41 +1,83 @@
 const createPerson = (name, age) => {
-  // your code here
+  return { name: name, age: age };
 };
 
 const getName = (object) => {
-  // your code here
+  return object.name;
 };
 
 const getProperty = (property, object) => {
-  // your code here
+  return object[property];
 };
 
 const hasProperty = (property, object) => {
-  // your code here
+  if (property in object) {
+    return true;
+  }
+  return false;
 };
 
 const isOver65 = (person) => {
-  // your code here
+  if (person.age > 65) {
+    return true;
+  }
+  return false;
 };
 
 const getAges = (people) => {
-  // your code here
+  const agesArray = [];
+  for (let i = 0; i < people.length; i++) {
+    agesArray.push(people[i].age);
+  }
+  return agesArray;
 };
 
 const findByName = (name, people) => {
-  // your code here
+  // const filteredNames = [];
+  // for (let i = 0; i < people.length; i++) {
+  //   // console.log(people.name);
+  //   if (this.name === name) {
+  //     filteredNames.push(people[i]);
+  //   }
+  // }
+
+  return people.find((person) => {
+    return person.name === name;
+  });
 };
 
 const findHondas = (cars) => {
-  // your code here
+  const hondas = [];
+  for (let i = 0; i < cars.length; i++) {
+    if (cars[i].manufacturer === 'Honda') {
+      hondas.push(cars[i]);
+    }
+  }
+  return hondas;
 };
 
 const averageAge = (people) => {
-  // your code here
+  const agesArray = [];
+  for (let i = 0; i < people.length; i++) {
+    agesArray.push(people[i].age);
+  }
+  let totalAge = 0;
+  for (let j = 0; j < agesArray.length; j++) {
+    totalAge += agesArray[j];
+  }
+  const averageAgeValue = totalAge / agesArray.length;
+  return averageAgeValue;
 };
 
 const createTalkingPerson = (name, age) => {
-  // your code here
+  
+  function sayHello() {
+    const sayHello1 =  'Hi ' + this.name;
+  }
+
+  name.sayHello1();
+
+  sayHello1 += ' my name is' + name + ' and I am 40!';
 };
 
 module.exports = {
