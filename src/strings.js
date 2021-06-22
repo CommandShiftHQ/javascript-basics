431d9cb7bbe17dcc600c7a6b09802484bcd3b579
adacameron <ada.e.cameron@gmail.com>
2021-06-22 20:42:37 +0100

    strings.js complete

--- src/strings.js
+++ src/strings.js
@@ -1,26 +1,25 @@
 const sayHello = string => {
-  let world = "Hello, world!";
-  return "Hello, world!";
-}
+  return ("Hello, " + string + "!");
+};
 
 const uppercase = string => {
-  // your code here
+  return string.toUpperCase(); 
 };
 
 const lowercase = string => {
-  // your code here
+  return string.toLowerCase();
 };
 
 const countCharacters = string => {
-  // your code here
+  return string.length;
 };
 
 const firstCharacter = string => {
-  // your code here
+  return string.charAt(0);
 };
 
 const firstCharacters = (string, n) => {
-  // your code here
+  return string.substring(0, n);
 };
 
 module.exports = {
