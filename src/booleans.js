f86e69fc07c4ea725567f24180ab68fd7a94a695
Tom Ramsden <thomas.ramsden20@gmail.com>
2021-06-26 13:42:57 +0100

    Completed all of the boolean katas

--- src/booleans.js
+++ src/booleans.js
@@ -1,61 +1,61 @@
 const negate = a => {
-  // your code here
+  return !a;
 };
 
 const both = (a, b) => {
-  // your code here
+  return a && b;
 };
 
 const either = (a, b) => {
-  // your code here
+  return a || b;
 };
 
 const none = (a, b) => {
-  // your code here
+  return !a && !b;
 };
 
 const one = (a, b) => {
-  // your code here
+  return a !== b;
 };
 
 const truthiness = a => {
-  // your code here
+  return !!a;
 };
 
 const isEqual = (a, b) => {
-  // your code here
+  return a === b;
 };
 
 const isGreaterThan = (a, b) => {
-  // your code here
+  return a > b;
 };
 
 const isLessThanOrEqualTo = (a, b) => {
-  // your code here
+  return a <= b;
 };
 
 const isOdd = a => {
-  // your code here
+  return a % 2 !== 0;
 };
 
 const isEven = a => {
-  // your code here
+  return a % 2 === 0;
 };
 
 const isSquare = a => {
-  // your code here
+  return Number.isInteger(Math.sqrt(a));
 };
 
 const startsWith = (char, string) => {
-  // your code here
+  return string[0] === char;
 };
 
 const containsVowels = string => {
-  // your code here
+  return Boolean(string.match(/[aeiou]/i));
 };
 
 const isLowerCase = string => {
-  // your code here
+  return string.toLowerCase() === string;
 };
 
 module.exports = {
