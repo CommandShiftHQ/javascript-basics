718715d81edb6a6f7caa77c905c19c146096efb6
Tom Ramsden <thomas.ramsden20@gmail.com>
2021-06-26 15:23:20 +0100

    Completed 11/17 array katas

--- src/arrays.js
+++ src/arrays.js
@@ -1,41 +1,41 @@
 const getNthElement = (index, array) => {
-  // your code here
+  return array[index % (array.length)];
 };
 
 const arrayToCSVString = array => {
-  // your code here
+  return array.join();
 };
 
 const csvStringToArray = string => {
-  // your code here
+  return string.split(",");
 };
 
 const addToArray = (element, array) => {
-  // your code here
+  array.push(element);
 };
 
 const addToArray2 = (element, array) => {
-  // your code here
+  return [...array, element];
 };
 
 const removeNthElement = (index, array) => {
-  // your code here
+  array.splice(index, 1);
 };
 
 const numbersToStrings = numbers => {
-  // your code here
+  return numbers.map(String);
 };
 
 const uppercaseWordsInArray = strings => {
-  // your code here
+  return strings.map(toUpper => toUpper.toUpperCase());
 };
 
-const reverseWordsInArray = strings => {
-  // your code here
-};
+const reverseWordsInArray = strings => strings.map(newString => {
+  return newString.split("").reverse().join("");
+});
 
 const onlyEven = numbers => {
-  // your code here
+  return numbers.filter(number => number % 2 === 0);
 };
 
 const removeNthElement2 = (index, array) => {
