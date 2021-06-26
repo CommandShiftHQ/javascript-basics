6d248af01950a79ac7bb868689811d7f1604a152
Tom Ramsden <thomas.ramsden20@gmail.com>
2021-06-26 12:07:58 +0100

    Completed all of the string katas

--- src/strings.js
+++ src/strings.js
@@ -1,25 +1,25 @@
 const sayHello = string => {
-  return "Hello, world!";
+  return `Hello, ${string}!`
 };
 
 const uppercase = string => {
-  // your code here
+  return string.toUpperCase();
 };
 
 const lowercase = string => {
-  // your code here
+  return string.toLowerCase();
 };
 
 const countCharacters = string => {
-  // your code here
+  return string.length;
 };
 
 const firstCharacter = string => {
-  // your code here
+  return string[0];
 };
 
 const firstCharacters = (string, n) => {
-  // your code here
+  return string.substring(0, n);
 };
 
 module.exports = {
