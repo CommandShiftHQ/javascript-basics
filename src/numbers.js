5880f242489d1b19462ca60eae7d491438378a1e
Shaun Emery <shaunsheep18@gmail.com>
2021-11-22 15:03:44 +0000

    numbers exercise complete

--- src/numbers.js
+++ src/numbers.js
@@ -13,35 +13,35 @@ function multiply (a, b) {
 }
 
 function divide (a, b) {
-  // your code here
+  return a/b;
 }
 
 function power (a, b) {
-  // your code here
+  return a**b;
 }
 
 function round (a) {
-  // your code here
+  return Math.round(a);
 }
 
 function roundUp (a) {
-  // your code here
+  return Math.ceil(a);
 }
 
 function roundDown (a) {
-  // your code here
+  return Math.floor(a);
 }
 
 function absolute (a) {
-  // your code here
+  return Math.abs(a);
 }
 
 function quotient (a, b) {
-  // your code here
+  return ~~(a/b);
 }
 
 function remainder (a, b) {
-  // your code here
+  return a % b;
 }
 
 module.exports = {
