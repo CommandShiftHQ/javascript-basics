aa859fe1cabdd362feeab19517be586d7660e737
Shaun Emery <shaunsheep18@gmail.com>
2021-11-23 10:13:50 +0000

    halfway through boolean

--- src/booleans.js
+++ src/booleans.js
@@ -1,17 +1,17 @@
 function negate(a) {
-  // your code here
+  return !a;
 };
 
 function both(a, b) {
-  // your code here
+  return (a && b);
 };
 
 function either(a, b) {
-  // your code here
+  return a || b;
 };
 
 function none(a, b) {
-  // your code here
+  return !(a || b);
 };
 
 function one(a, b) {
@@ -19,27 +19,31 @@ function one(a, b) {
 };
 
 function truthiness(a) {
-  // your code here
+  return Boolean(a);
 };
 
 function isEqual(a, b) {
-  // your code here
+  return a === b;
 };
 
 function isGreaterThan(a, b) {
-  // your code here
+  return a > b;
 };
 
 function isLessThanOrEqualTo(a, b) {
-  // your code here
+  return a <= b
 };
 
 function isOdd(a) {
-  // your code here
+  if (a%2 !== 0){
+  return true;
+  }
 };
 
 function isEven(a) {
-  // your code here
+  if (a%2 === 0) {
+  return true;
+  }
 };
 
 function isSquare(a) {
