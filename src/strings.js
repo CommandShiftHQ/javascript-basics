fc7983be9bdbb3225adec3b9cffa60c28c867138
Dami <dami.fajinmi@hotmail.com>
2021-11-24 18:11:17 +0000

    strings v1

--- src/strings.js
+++ src/strings.js
@@ -1,25 +1,25 @@
-function sayHello (string) {
-  // return( "Hello" + string!)
+function sayHello(string) {
+  return "Hello" + string;
 };
 
-function uppercase (string) {
-  // your code here
+function uppercase(string) {
+  return string.toUpperCase();
 };
 
-function lowercase (string) {
-  // your code here
+function lowercase(string) {
+  return string.toLowerCase();
 };
 
-function countCharacters (string) {
-  // your code here
+function countCharacters(string) {
+  return string.length;
 };
 
-function firstCharacter (string) {
-  // your code here
+function firstCharacter(string) {
+  return string.charAt(1);
 };
 
-function firstCharacters (string, n) {
-  // your code here
+function firstCharacters(string, n) {
+  return string.charAt(n);
 };
 
 module.exports = {
