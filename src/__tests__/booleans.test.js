56364431d06734235f924b0afd958da1e4157ae8
Romy <33502097+anywhereiromy@users.noreply.github.com>
2021-11-28 17:43:19 +0000

    Update booleans.test.js
    Add two test cases for startsWith

--- src/__tests__/booleans.test.js
+++ src/__tests__/booleans.test.js
@@ -130,6 +130,8 @@ describe('isSquare', () => {
 describe('startsWith', () => {
   xit('returns whether the given string starts with the given character', () => {
     expect(startsWith('a', 'aardvark')).toBe(true);
+    expect(startsWith('c', 'aardvark')).toBe(false);
+    expect(startsWith('b', 'baardvark')).toBe(true);
     expect(startsWith('a', 'qaardvark')).toBe(false);
     expect(startsWith('a', 'Aardvark')).toBe(false);
   });
