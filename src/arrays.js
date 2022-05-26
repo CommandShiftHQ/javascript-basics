d98730a534f00106a0ba868cd6a14b9656cdb1aa
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-05-26 21:44:55 +0100

    Make removeNthElement function pass removeNthElement test

--- src/arrays.js
+++ src/arrays.js
@@ -25,6 +25,7 @@ const addToArray2 = (element, array) => {
 
 const removeNthElement = (index, array) => {
   // your code here
+  return array.splice(index, 1);
 };
 
 const numbersToStrings = numbers => {
