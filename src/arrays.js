7dfdcbd4f7da5084fcfeb450cd0859aa11262a0e
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-05-26 22:40:17 +0100

    Remove redundant comments in arrays.js

--- src/arrays.js
+++ src/arrays.js
@@ -1,68 +1,55 @@
 const getNthElement = (index, array) => {
-  // your code here
   return array.at(index % array.length);
 };
 
 const arrayToCSVString = array => {
-  // your code here
   return array.join(',');
 };
 
 const csvStringToArray = string => {
-  // your code here
   return string.split(',');
 };
 
 const addToArray = (element, array) => {
-  // your code here
   array.push(element);
 };
 
 const addToArray2 = (element, array) => {
-  // your code here
   return [...array, element];
 };
 
 const removeNthElement = (index, array) => {
-  // your code here
   return array.splice(index, 1);
 };
 
 const numbersToStrings = numbers => {
-  // your code here
   return numbers.map(number => `${number}`);
 };
 
 const uppercaseWordsInArray = strings => {
-  // your code here
   return strings.map(string => string.toUpperCase());
 };
 
 const reverseWordsInArray = strings => {
-  // your code here
   return strings.map(string => [...string].reverse().join(''));
 };
 
 const onlyEven = numbers => {
-  // your code here
   return numbers.filter(number => number % 2 === 0);
 };
 
 const removeNthElement2 = (index, array) => {
-  // your code here
   const copyArray = [...array];
   copyArray.splice(index, 1);
   return copyArray;
 };
 
 const elementsStartingWithAVowel = strings => {
-  // your code here
   const regex = new RegExp('^[aeiou]', 'i');
   return strings.filter(string => regex.test(string));
 };
 
 const removeSpaces = string => {
-  // your code here
   return [...string]
     .reduce((acc, curr) => {
       if (curr !== ' ') {
@@ -74,12 +61,10 @@ const removeSpaces = string => {
 };
 
 const sumNumbers = numbers => {
-  // your code here
   return numbers.reduce((acc, curr) => acc + curr, 0);
 };
 
 const sortByLastLetter = strings => {
-  // your code here
   return strings.sort((stringA, stringB) =>
     stringA.at(-1).localeCompare(stringB.at(-1))
   );
