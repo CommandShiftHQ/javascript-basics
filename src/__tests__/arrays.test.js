0fa209acec2278e8c7c7de9c05f5d7564b96f3c5
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-05-26 22:25:09 +0100

    Unskip sortByLastLetter test

--- src/__tests__/arrays.test.js
+++ src/__tests__/arrays.test.js
@@ -233,7 +233,7 @@ describe('sumNumbers', () => {
 });
 
 describe('sortByLastLetter', () => {
-  xit('sorts the string by the last character', () => {
+  it('sorts the string by the last character', () => {
     expect(
       sortByLastLetter(['Lannister', 'Stark', 'Greyjoy', 'Targaryen'])
     ).toEqual(['Stark', 'Targaryen', 'Lannister', 'Greyjoy']);
