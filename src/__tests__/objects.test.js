0d31f63245f04d56c7aa048e3378afd2ba3705b9
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-05-27 10:36:41 +0100

    Unskip 'isOver65' test

--- src/__tests__/objects.test.js
+++ src/__tests__/objects.test.js
@@ -79,7 +79,7 @@ describe('hasProperty', () => {
 });
 
 describe('isOver65', () => {
-  xit('returns true if the person is aged over 65', () => {
+  it('returns true if the person is aged over 65', () => {
     const jim = {
       name: 'Jim',
       age: 66
