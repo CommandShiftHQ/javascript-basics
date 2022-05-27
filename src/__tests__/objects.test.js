756b705da7d87b62cca88c61f1673d67bfec6c92
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-05-27 10:44:54 +0100

    Unskip 'createTalkingPerson' test

--- src/__tests__/objects.test.js
+++ src/__tests__/objects.test.js
@@ -207,7 +207,7 @@ describe('averageAge', () => {
 });
 
 describe('createTalkingPerson', () => {
-  xit('returns a person who can introduce themselves', () => {
+  it('returns a person who can introduce themselves', () => {
     const bill = createTalkingPerson('Bill', 40);
     const catherine = createTalkingPerson('Catherine', 21);
     expect(bill).toEqual({
