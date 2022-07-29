8e891f2351d6e75e20de96f9ddd10b7db1e4b729
mike-shields-dev <mr.m.p.shields@gmail.com>
2022-07-29 21:10:58 +0100

    objects

--- src/objects.js
+++ src/objects.js
@@ -1,41 +1,50 @@
 const createPerson = (name, age) => {
-  // your code here
+  return {
+    name,
+    age
+  };
 };
 
 const getName = object => {
-  // your code here
+  return object.name;
 };
 
 const getProperty = (property, object) => {
-  // your code here
+  return object[property];
 };
 
 const hasProperty = (property, object) => {
-  // your code here
+  return property in object;
 };
 
 const isOver65 = person => {
-  // your code here
+  return person.age > 65;
 };
 
 const getAges = people => {
-  // your code here
+  return people.map(person => person.age);
 };
 
 const findByName = (name, people) => {
-  // your code here
+  return people.find(person => person.name === name);
 };
 
 const findHondas = cars => {
-  // your code here
+  return cars.filter(car => car.manufacturer === 'Honda');
 };
 
 const averageAge = people => {
-  // your code here
+  return people.reduce((acc, person) => acc + person.age, 0) / people.length;
 };
 
 const createTalkingPerson = (name, age) => {
-  // your code here
+  return {
+    name,
+    age,
+    introduce(otherName) {
+      return `Hi ${otherName}, my name is ${name} and I am ${age}!`;
+    }
+  };
 };
 
 module.exports = {
