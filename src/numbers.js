518e0a86c98f43f15d103575329dc76e1e4011cf
Meera A <mirunaalini.alagarajah@gmail.com>
2023-06-20 20:37:29 +0100

    numbers js

--- src/numbers.js
+++ src/numbers.js
@@ -1,44 +1,55 @@
 function add (a, b) {
+  return a+b;
   // your code here
 }
 
 function subtract (a, b) {
+  return a-b;
   // your code here
 }
 
 function multiply (a, b) {
+  return a*b;
   // your code here
 }
 
 function divide (a, b) {
+  return a/b;
   // your code here
 }
 
 function power (a, b) {
+  return a**b;
   // your code here
 }
 
 function round (a) {
+  return Math.round(a);
   // your code here
 }
 
 function roundUp (a) {
+  return Math.ceil(a);
   // your code here
 }
 
 function roundDown (a) {
+  return Math.floor(a);
   // your code here
 }
 
 function absolute (a) {
+  return Math.abs(a);
   // your code here
 }
 
 function quotient (a, b) {
+  return Math.trunc(a/b);
   // your code here
 }
 
 function remainder (a, b) {
+  return a%b;
   // your code here
 }
 
