b0186f2bd4b88b2e238bf34ecae14c8be742fbb6
Meera A <mirunaalini.alagarajah@gmail.com>
2023-06-20 20:37:50 +0100

    booleans js

--- src/booleans.js
+++ src/booleans.js
@@ -1,60 +1,141 @@
 function negate(a) {
+  return !a;
   // your code here
 };
 
 function both(a, b) {
+  if (a===true && b===true){
+    return true;
+  }else{
+    return false;
+  }
   // your code here
-};
+}
 
 function either(a, b) {
+  if (a===true || b===true){
+    return true 
+  }else{ 
+    return false;
+    }
+  };
   // your code here
-};
+
 
 function none(a, b) {
+  if (a===true || b===true){
+    return false
+  }else{
+    return true;
+  }
   // your code here
 };
 
 function one(a, b) {
-  // your code here
-};
+  if(a===true && b===true){
+    return false;
+  }else if (a===true || b===true){
+  return true;
+  }else{
+      return false;
+    }
+  };
+  
+
 
 function truthiness(a) {
+  return Boolean(a);
+  }
+
+
   // your code here
-};
+
 
 function isEqual(a, b) {
+  if(a===b){
+    return true;
+  }else{
+    return false;
+  }
   // your code here
 };
 
 function isGreaterThan(a, b) {
+  if(a>b){
+    return true;
+  }else{
+    return false;
+  }
   // your code here
 };
 
 function isLessThanOrEqualTo(a, b) {
+  if (a<=b){
+    return true;
+  }else{
+    return false;
+  }
   // your code here
 };
 
 function isOdd(a) {
-  // your code here
+  if(a%2!==0){
+    return true;
+  }else{
+    return false;
+  }
+ 
 };
 
 function isEven(a) {
+  if(a%2===0){
+    return true;
+  }else{
+    return false;
+  }
+
   // your code here
 };
 
 function isSquare(a) {
+  if (Number.isInteger(Math.sqrt(a))){
+    return true;
+  }else{
+    return false;
+  }
   // your code here
 };
 
 function startsWith(char, string) {
+  if(string.startsWith(char)){
+    return true;
+  }else {
+    return false;
+  }
   // your code here
 };
 
 function containsVowels(string) {
-  // your code here
-};
+ 
+    const vowels = ["a" ,"e" ,"i", "o","u","A","B","C","D","E"];
+    let result=false;
+    for(let i=0;i<string.length;i++)
+    if(vowels.includes(string[i])){
+      result=true;
+    }
+    return result;
+  }
+
+
+
+
 
 function isLowerCase(string) {
+  if(string===string.toLowerCase()){
+    return true;
+  }else{
+    return false;
+  }
   // your code here
 };
 
