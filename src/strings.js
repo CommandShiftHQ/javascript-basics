4cfb5d3430e85d702683aff84a8dd300a37dc152
Meera A <mirunaalini.alagarajah@gmail.com>
2023-06-20 20:36:30 +0100

    strings js

--- src/strings.js
+++ src/strings.js
@@ -1,24 +1,30 @@
 function sayHello (string) {
+  return "Hello, " + string + "!";
   // your code here
 };
 
 function uppercase (string) {
+  return string.toUpperCase();
   // your code here
 };
 
 function lowercase (string) {
+  return string.toLowerCase();
   // your code here
 };
 
 function countCharacters (string) {
+  return string.length;
   // your code here
 };
 
 function firstCharacter (string) {
+  return string.charAt(0);
   // your code here
 };
 
 function firstCharacters (string, n) {
+  return string.substring(0,n);
   // your code here
 };
 
