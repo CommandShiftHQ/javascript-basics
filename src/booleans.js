e79e15c443e8894b0514dfc371403b2bd8c7b71b
dylstar135 <dylstar160990@googlemail.com>
2023-08-17 20:11:52 +0100

    booleans

--- src/booleans.js
+++ src/booleans.js
@@ -52,39 +52,40 @@ function truthiness(a) {
 };
 
 function isEqual(a, b) {
-  // your code here
+  return a===b;
 };
 
 function isGreaterThan(a, b) {
-  // your code here
+  return a > b;
 };
 
 function isLessThanOrEqualTo(a, b) {
-  // your code here
+  return a <= b;
 };
 
 function isOdd(a) {
-  // your code here
+  return a % 2 !== 0;
 };
 
 function isEven(a) {
-  // your code here
+  return a % 2 === 0;
 };
 
 function isSquare(a) {
-  // your code here
+ const sqrt = Math.sqrt(a);
+ return sqrt === Math.floor(sqrt);
 };
 
 function startsWith(char, string) {
-  // your code here
+  return string.charAt(0) === char;
 };
 
 function containsVowels(string) {
-  // your code here
+  return /[aeiouAEIOU]/.test(string);
 };
 
 function isLowerCase(string) {
-  // your code here
+  return string === string.toLowerCase();
 };
 
 module.exports = {
